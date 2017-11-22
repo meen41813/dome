3b986a7683c05aeb6d08b2e2d8ee44e59a5380e8
Yodyeam Autanuwat <31239275+meen41813@users.noreply.github.com>
2017-11-22 18:32:31 +0700

    update database

--- myDorm.sql
+++ myDorm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2017 at 10:15 PM
+-- Generation Time: Nov 22, 2017 at 12:20 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.10
 
@@ -32,15 +32,17 @@ CREATE TABLE `admin` (
   `telephone` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `address` text COLLATE utf8_unicode_ci NOT NULL,
   `electricRate` int(100) NOT NULL,
-  `waterRate` int(100) NOT NULL
+  `waterRate` int(100) NOT NULL,
+  `username` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(15) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`telephone`, `address`, `electricRate`, `waterRate`) VALUES
-('081-234-5678', '123/456 Charongkrung Rd., Ladkrabang, Bangkok, Thailand 10520', 8, 18);
+INSERT INTO `admin` (`telephone`, `address`, `electricRate`, `waterRate`, `username`, `password`) VALUES
+('081-234-5678', '123/456 Charongkrung Rd., Ladkrabang, Bangkok, Thailand 10520', 8, 18, 'admin', 'mydorm');
 
 -- --------------------------------------------------------
 
@@ -82,20 +84,21 @@ CREATE TABLE `user` (
   `address` text COLLATE utf8_unicode_ci NOT NULL,
   `contract` date NOT NULL,
   `picture` text COLLATE utf8_unicode_ci NOT NULL,
-  `total` int(11) NOT NULL
+  `total` int(11) NOT NULL,
+  `moveout` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`userID`, `username`, `password`, `name`, `nickname`, `personalID`, `address`, `contract`, `picture`, `total`) VALUES
-('0001', 'titlez', 'ilovese', 'Jeerakitti Niamlarp', 'Title', '1234567890123', '132/36 Rama XI rd., Samsen-Nai, Phayathai, Bangkok, Thailand 10400', '2018-08-31', 'url', 5626),
-('0002', 'mormix', 'ilikese', 'Tuchchapol Tuanghirunvimon', 'Mix', '3210987654321', '24 soi petkasem 62/1 bangkae bangkok 10160', '2018-06-30', 'url', 0),
-('0003', 'mew_maro', 'lazygirl', 'Mew Maro', 'Mew', '1092355555000', 'Sapran loi', '2018-02-28', 'url', 6002),
-('0004', 'nut_eieiza', 'javaboss', 'Nut TheJavaBoy', 'Nut', '1111111111111', 'ESL', '2018-03-31', 'url', 5812),
-('0005', 'meenza', 'meeneiei', 'Meen Eiei', 'Meen', '2222222222222', 'Mai roo ah', '2018-03-31', 'url', 0),
-('0006', 'monandchin', 'cinnamon', 'Mon Chin', 'Cinnamon', '5555555555555', 'Make', '2018-01-31', 'url', 0);
+INSERT INTO `user` (`userID`, `username`, `password`, `name`, `nickname`, `personalID`, `address`, `contract`, `picture`, `total`, `moveout`) VALUES
+('0001', 'titlez', 'ilovese', 'Jeerakitti Niamlarp', 'Title', '1234567890123', '132/36 Rama XI rd., Samsen-Nai, Phayathai, Bangkok, Thailand 10400', '2018-08-31', 'url', 0, '2017-12-27'),
+('0002', 'mormix', 'ilikese', 'Tuchchapol Tuanghirunvimon', 'Mix', '3210987654321', '24 soi petkasem 62/1 bangkae bangkok 10160', '2018-06-30', 'url', 0, NULL),
+('0003', 'mew_maro', 'lazygirl', 'Mew Maro', 'Mew', '1092355555000', 'Sapran loi', '2018-02-28', 'url', 0, NULL),
+('0004', 'nut_eieiza', 'javaboss', 'Nut TheJavaBoy', 'Nut', '1111111111111', 'ESL', '2018-03-31', 'url', 5812, NULL),
+('0005', 'meenza', 'meeneiei', 'Meen Eiei', 'Meen', '2222222222222', 'Mai roo ah', '2018-03-31', 'url', 0, NULL),
+('0006', 'monandchin', 'cinnamon', 'Mon Chin', 'Cinnamon', '5555555555555', 'Make', '2018-01-31', 'url', 0, NULL);
 
 --
 -- Indexes for dumped tables
